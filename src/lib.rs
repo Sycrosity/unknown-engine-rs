849edcab93457ac7e7441fff8bbc11a91eafe8b3
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2022-08-15 16:17:41 +0200

    generate a simple window that can be closed with a key press.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
+pub fn run() {
+    env_logger::init();
+    let event_loop: EventLoop<()> = EventLoop::new();
+    let window: Window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(
+        move |event: Event<()>, _, control_flow: &mut ControlFlow| match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => match event {
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                _ => {}
+            },
+            _ => {}
+        },
+    );
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
+use un_known_engine::run;
 
 fn main() {
-
-
-
+    run();
 }
