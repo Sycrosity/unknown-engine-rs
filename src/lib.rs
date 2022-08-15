4370723264bf8cc975664680b4687a1495e9c80c
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2022-08-15 16:36:27 +0200

    commented

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,28 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
+//run the rasterizer
 pub fn run() {
+    //wgpu doesn't use normal error logging, requires env_logger for its custom error messages
     env_logger::init();
+
+    //a way to retrive events sent by the system, and windows registed into the event loop
     let event_loop: EventLoop<()> = EventLoop::new();
+    //a window that can be manipulated to draw on the screen - in init it gets added to the event loop by the window builder
     let window: Window = WindowBuilder::new().build(&event_loop).unwrap();
 
+    //starts the event loop
     event_loop.run(
         move |event: Event<()>, _, control_flow: &mut ControlFlow| match event {
             Event::WindowEvent {
                 ref event,
                 window_id,
             } if window_id == window.id() => match event {
+                //system requested window closing
                 WindowEvent::CloseRequested
+                //any keyboard input
                 | WindowEvent::KeyboardInput {
+                    //if escape is pressed, the window will close.
                     input:
                         KeyboardInput {
                             state: ElementState::Pressed,
@@ -32,6 +41,7 @@ pub fn run() {
     );
 }
 
+//[TODO] create real tests for the program
 #[cfg(test)]
 mod tests {
     #[test]
